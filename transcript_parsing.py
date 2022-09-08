371ba759f5dcfed535c2d71f3c63767b736ee418
flambuth <flambuth@gmail.com>
2022-09-08 14:28:15 -0500

    i need to add the counts for each name into a total

--- transcript_parsing.py
+++ transcript_parsing.py
@@ -5,14 +5,86 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import os
 
-s1_episodes = os.listdir('transcripts/')[:14]
 
-season_1_words = 'mad'
+seasons = {
+'s1_episodes' : os.listdir('transcripts/')[:13],
+'s2_episodes' : os.listdir('transcripts/')[13:26],
+'s3_episodes' : os.listdir('transcripts/')[26:39],
+'s4_episodes' : os.listdir('transcripts/')[39:52],
+'s5_episodes' : os.listdir('transcripts/')[52:64],
+'s6_episodes' : os.listdir('transcripts/')[64:77],
+'s7_episodes' : os.listdir('transcripts/')[77:],
+}
 
+
+
+names = {
+'don' : ['don','donald','draper'],
+'peggy' : ['peggy','olson','margaret'], 
+'pete'  : ['pete','peter','campbell'],
+'roger' : ['roger','sterling'],
+'betty' : ['betty','bets','birdy'],
+'joan' : ['joan','harris','holloway'],
+'crane' : ['harry', 'crane'],
+'kinsey' : ['paul','kinsey']
+}
+
+
+class mad_men_words:
+    def __init__(self):
+        self.episode_list = os.listdir('transcripts/')
+        self.char_names = names
+        self.seasons = seasons
+
+    def season_word_counts(self, season):
+        season_words = 'test'
+
+        for title in season:
+            with open(f'transcripts/{title}') as f:
+                big_word = f.readlines()
+                season_words += big_word[0] 
+
+        s1_counter = Counter(season_words.split(' '))
+        count_of_words = s1_counter.most_common()
+        return count_of_words
+
+class mad_men_season(mad_men_words):
+    def __init__(self, season_episodes):
+        super().__init__()
+        self.season_episodes = season_episodes
+        self.word_counts = self.season_word_counts(season_episodes)
+    
+    def search_wordcount(self, terms):
+        return [i for i in self.word_counts if i[0] in terms]
+
+    def name_counts_per_season(self):
+        name_book = {}
+
+        for name in self.char_names:
+            name_book[name] = self.search_wordcount(self.char_names[name])
+        return name_book
+####
+
+
+s1_obj = mad_men_season(seasons['s1_episodes'])
+#s1_obj.search_wordcount(s1_obj.char_names['betty'])
+
+
+
+'''
 for title in s1_episodes:
     with open(f'transcripts/{title}') as f:
         big_word = f.readlines()
         season_1_words += big_word[0] 
 
 s1_counter = Counter(season_1_words.split(' '))
-s1_counter.most_common()
+season_one_word_counts = s1_counter.most_common()
+
+
+season1 = mm_obj.seasons['s1_episodes']
+season2 = mm_obj.seasons['s2_episodes']
+#season_word_counts(season_transcripts)
+
+woah = mm_obj.season_word_counts(season1)
+awesome = mm_obj.season_word_counts(season2)
+'''
